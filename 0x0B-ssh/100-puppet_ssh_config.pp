ff452545490f375765d6e2c3eb30b7d6bb9183c8
waweroh <scadmsez@gmail.com>
2022-09-22 05:32:29 +0300

    configure ssh using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# no password authentication change
+file_line { 'Turn off passwd auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '#   PasswordAuthentication no',
+    match  => '#   PasswordAuthentication yes'
+}
+
+file_line { 'Declare identity file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '#   IdentityFile ~/.ssh/school',
+    match  => '#   IdentityFile ~/.ssh/id_rsa'
+}
